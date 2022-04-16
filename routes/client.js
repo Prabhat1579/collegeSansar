35905f7e2c7f1e5045b7064a4fcf67336bed849c
Kritish Bhattarai <rishi58rishi@gmail.com>
2022-04-16 19:26:36 +0545

    feat: view colleges

--- routes/client.js
+++ routes/client.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const College = require('../model/College');
 
 const router = express.Router();
 
@@ -8,8 +9,17 @@ router.get('/', (req, res) => {
    res.render('index', { username, isLoggedIn });
 });
 
-router.get('/college', (req, res) => {
-   res.render('college');
+router.get('/college', async (req, res) => {
+   const collegeList = await College.findAll();
+
+   res.render('college', {
+      colleges: collegeList.map(({ name, fee, description, thumbnail }) => ({
+         name,
+         fee,
+         description,
+         img: `assets/${thumbnail}`,
+      })),
+   });
 });
 
 router.get('/exam', (req, res) => {
