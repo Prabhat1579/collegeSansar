2bad6b8b5258052bb69f2af062f8edc4c9c4951d
Kritish Bhattarai <kritish.bhattarai@bigtincan.com>
2022-04-16 10:50:41 +0545

    added college model

---
+++ model/College.js
@@ -0,0 +1,26 @@
+// Include Sequelize module.
+const Sequelize = require('sequelize');
+
+const db = require('../config/db');
+const { sequelize } = db;
+
+const College = sequelize.define('college', {
+   id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+   },
+
+   name: { type: Sequelize.STRING, allowNull: false },
+
+   description: { type: Sequelize.STRING, allowNull: false },
+
+   fee: { type: Sequelize.STRING, allowNull: false },
+
+   isAmongTopColleges: { type: Sequelize.BOOLEAN, defaultValue: false },
+
+   date: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
+});
+
+module.exports = College;
