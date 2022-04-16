f92c250f46d20854f9a6a5b77f09547677c92f86
Kritish Bhattarai <kritish.bhattarai@bigtincan.com>
2022-04-16 11:54:04 +0545

    feat: added admin model

---
+++ model/Admin.js
@@ -0,0 +1,24 @@
+// Include Sequelize module.
+const Sequelize = require('sequelize');
+
+const db = require('../config/db');
+const { sequelize } = db;
+
+const Admin = sequelize.define('admin', {
+   id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+   },
+
+   name: { type: Sequelize.STRING, allowNull: false },
+
+   email: { type: Sequelize.STRING, allowNull: false },
+
+   password: { type: Sequelize.STRING, allowNull: false },
+
+   date: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
+});
+
+module.exports = Admin;
