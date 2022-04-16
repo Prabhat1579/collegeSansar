503187e6e9908cd8f3e0237d8b46fb5f9228755e
Kritish Bhattarai <kritish.bhattarai@bigtincan.com>
2022-04-16 12:35:11 +0545

    added admin views

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/admin', (req, res) => {});
+// * GET ROUTES
+
+router.get('/', (req, res) => {});
+
+router.get('/college', (req, res) => {
+   res.render('admin_college');
+});
+
+router.get('/career', (req, res) => {
+   res.render('admin_career');
+});
+
+router.get('/exam', (req, res) => {
+   res.render('admin_exam');
+});
+
+router.get('/login', (req, res) => {
+   res.render('admin_login');
+});
+
+router.get('/register', (req, res) => {
+   res.render('admin_register');
+});
+
+router.get('/logout', (req, res) => {});
+
+// * POST ROUTES
+
+router.post('/login', (req, res) => {});
+
+router.post('/register', (req, res) => {});
 
 router.post('/college', (req, res) => {});
 
-router.post('/exam', (req, res) => {});
-
 router.post('/career', (req, res) => {});
+
+router.post('/exam', (req, res) => {});
 
 module.exports = router;
---
+++ seeders/colleges.js
@@ -0,0 +1,17 @@
+const colleges = [
+   {
+      name: 'Kathmandu University',
+      fee: '60000 per year',
+      image: 'KathmanduUniversity.png',
+   },
+   {
+      name: 'Nepal Engineering College',
+      fee: '50000 per year',
+      image: 'NepalEngineeringcollege.png',
+   },
+   {
+      name: 'Pokhara University',
+      fee: '80000 per year',
+      image: 'PokharaUniversity.png',
+   },
+];
